f4e9cb32202e7bd965c853de778ac7ef9f8c95a5
marcel-mathias-nolte <github@noltecomputer.de>
2022-03-02 11:22:11 +0100

    first

--- src/WatermarkService.php
+++ src/WatermarkService.php
@@ -15,25 +15,84 @@ namespace MarcelMathiasNolte\WatermarkBundle;
 use Contao\CoreBundle\ServiceAnnotation\Hook;
 use Contao\Image;
 use Contao\File;
+use OMOSde\ContaoOmImagineBundle\Imagine;
+use OMOSde\ContaoOmImagineBundle\OmImagineModel;
 
 /**
  * @Hook("getImage")
  */
-class WatermarkService
+class WatermarkService extends Imagine
 {
     public function __invoke(
-        string $originalPath,
-        int $width,
-        int $height,
-        string $mode,
-        string $cacheName,
-        File $file,
-        string $targetPath,
-        Image $imageObject
-    ): ?string
+        $originalPath,
+        $width,
+        $height,
+        $mode = '',
+        $target = null,
+        $force = false
+    )
     {
-        // Return the path to a custom image
-        var_dump(func_get_args()); die();
+        $objImagine = OmImagineModel::findBy(['published=?', 'directory<>""'], [1]);
+        if (!is_object($objImagine))
+        {
+            return null;
+        }
+        // do for all active manipulations
+        foreach ($objImagine as $objManipulation)
+        {
+            // get all active actions for this manipulation and check next if none exists
+            $objActions = OmImagineActionModel::findBy(['pid=?', 'active=1'], [$objManipulation->id], ['order' => 'sorting ASC']);
+            if (!$objActions)
+            {
+                continue;
+            }
+
+            // check for directories
+            $objDirectories = \FilesModel::findMultipleByIds(deserialize($objManipulation->directory, true));
+            if (!$objDirectories)
+            {
+                continue;
+            }
+
+            // create an array with the target directories
+            foreach ($objDirectories as $directory)
+            {
+                $arrDirectories[] = $directory->path;
+            }
+
+            $strFile = TL_ROOT . '/' . $originalPath;
+
+            // check if the file exists
+            if (!file_exists($strFile))
+            {
+                continue;
+            }
+            if (!file_exists(TL_ROOT . '/' . $target))
+            {
+                continue;
+            }
+
+            // get path info of file
+            $arrPathInfo = pathinfo($strFile);
+
+            // check file extension
+            if (!in_array(strtolower($arrPathInfo['extension']), ['gif', 'jpg', 'png']))
+            {
+                continue;
+            }
+
+            //
+            foreach ($arrDirectories as $strDirectory)
+            {
+                //
+                if (!strpos($arrPathInfo['dirname'], $strDirectory) !== false)
+                {
+                    continue;
+                }
+
+                self::handleActions(TL_ROOT . '/' . $target, $objManipulation, $objActions);
+            }
+        }
 
         return null;
     }
